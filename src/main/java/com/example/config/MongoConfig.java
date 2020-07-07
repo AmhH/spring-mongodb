8f5e7a7ae65438fcc2b291a8f697cc5af1f3aa17
Amh <abaams78@gmail.com>
2020-07-07 13:29:37 -0500

    mongo template and repo

---
+++ src/main/java/com/example/config/MongoConfig.java
@@ -0,0 +1,59 @@
+package com.example.config;
+
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Configuration
+@EnableMongoRepositories(basePackages = "com.example.repository")
+public class MongoConfig extends AbstractMongoConfiguration {
+
+    //private final List<Converter<?, ?>> converters = new ArrayList<Converter<?, ?>>();
+
+    @Override
+    protected String getDatabaseName() {
+        return "test";
+    }
+
+    @Override
+    public MongoClient mongoClient() {
+        return new MongoClient("127.0.0.1", 27017);
+    }
+
+    @Override
+    public Collection<String> getMappingBasePackages() {
+        return Collections.singletonList("com.example");
+    }
+
+    /*@Bean
+    public UserCascadeSaveMongoEventListener userCascadingMongoEventListener() {
+        return new UserCascadeSaveMongoEventListener();
+    }
+
+    @Bean
+    public CascadeSaveMongoEventListener cascadingMongoEventListener() {
+        return new CascadeSaveMongoEventListener();
+    }
+
+    @Override
+    public MongoCustomConversions customConversions() {
+        converters.add(new UserWriterConverter());
+        converters.add(new ZonedDateTimeReadConverter());
+        converters.add(new ZonedDateTimeWriteConverter());
+        return new MongoCustomConversions(converters);
+    }
+
+    @Bean
+    public GridFsTemplate gridFsTemplate() throws Exception {
+        return new GridFsTemplate(mongoDbFactory(), mappingMongoConverter());
+    }
+
+    @Bean
+    MongoTransactionManager transactionManager(MongoDbFactory dbFactory) {
+        return new MongoTransactionManager(dbFactory);
+    }*/
+}
